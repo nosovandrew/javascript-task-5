b2c169331819cb0ff018a9fe173e62366362268a
Андрей Носов <andreinosov@MacBook-Air-Andrej.local>
2017-11-01 22:31:40 +0500

    First try

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
@@ -12,6 +12,22 @@ module.exports = getEmitter;
  * @returns {Object}
  */
 function getEmitter() {
+    let subscriptions = {};
+
+    function parseEvents(event) {
+        let allEvents = [];
+        let parse = event.split('.');
+        let current = '';
+        for (let i = 0; i < parse.length; i++) {
+            current += '.' + parse[i];
+            if (parse[i]) {
+                allEvents.push(current.substring(1, current.length));
+            }
+        }
+
+        return allEvents.reverse();
+    }
+    
     return {
 
         /**
@@ -20,8 +36,14 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          */
+
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!subscriptions.hasOwnProperty(event)) {
+                subscriptions[event] = []
+            }
+            subscriptions[event].push({ context, handler });
+
+            return this;
         },
 
         /**
@@ -29,16 +51,37 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          */
+
         off: function (event, context) {
-            console.info(event, context);
+            let commands = Object.keys(subscriptions).filter(command =>
+                command.startsWith(event));
+            commands.forEach(command => {
+                for (let i = 0; i < subscriptions[command].length; i++) {
+                    if (subscriptions[command][i].context === context) {
+                        subscriptions[command].splice(i, 1);
+                    }
+                }
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
          */
+
         emit: function (event) {
-            console.info(event);
+            let commands = parseEvents(event);
+            for (let el of commands) {
+                if (subscriptions[el]) {
+                    subscriptions[el].forEach((sub) => {
+                        sub.handler.call(sub.context);
+                    });
+                }
+            }
+
+            return this;
         },
 
         /**
@@ -49,6 +92,7 @@ function getEmitter() {
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
          */
+
         several: function (event, context, handler, times) {
             console.info(event, context, handler, times);
         },
@@ -61,6 +105,7 @@ function getEmitter() {
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
          */
+
         through: function (event, context, handler, frequency) {
             console.info(event, context, handler, frequency);
         }
